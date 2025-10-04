6b25fa06a82257a6c29e18a00979901e34ebe3f5
7Bssk7 <arhip.fin3@gmail.com>
2025-10-04 10:35:10 -0700

    Update

--- 210-lab-19.cpp
+++ 210-lab-19.cpp
@@ -5,6 +5,32 @@
 #include <fstream>
 using namespace std;
 
+class MovieRating{
+    public:
+        string getComment() const { return comment; }
+        void setComment(string c) { this->comment = c; }
+        double getRating() const { return rating; }
+        void setRating( double r ) { this-> rating = r; }
+    private:
+        string comment;
+        double rating;
+};
+
+struct Node{
+    MovieRating review;
+    Node *next;
+
+};
+
+class Movie{
+    public:
+        string getTitle() const { return title; }
+        void setTitle( string t ) { this->title = t; }
+    private:
+        string title;
+        Node *list;
+};
+
 int main(){
 
     return 0;
